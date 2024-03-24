b293b772c5911c473e292fdaeb9bdc42e7c1d823
New <supagrit5@gmail.com>
2024-03-24 16:13:24 +0700

    client server connect

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,7 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [msg, setMsg] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios("http://localhost:3000");
+        setMsg(response.data); // Update msg with actual data
+      } catch (error) {
+        console.error(error); // Handle errors gracefully
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
       <h1 className="btn btn-primary">Hello world!</h1>
+      <br />
+      <button className="btn btn-secondary">{msg ? msg : "failed"}</button>
     </>
   );
 }
