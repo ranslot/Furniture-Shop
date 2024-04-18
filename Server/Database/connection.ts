d44625500e4716c30a4a98c7afbdadfd979ad1eb
New <supagrit5@gmail.com>
2024-04-18 23:14:26 +0700

    change usersModel and productModel naming stuff and refactor db to connection

--- Server/Database/connection.ts
+++ Server/Database/connection.ts
@@ -1,8 +1,12 @@
 import postgres from "postgres";
 import config from "../drizzle.config";
+import { drizzle } from "drizzle-orm/postgres-js";
+import * as schema from "../src/Models/Schema/databaseSchema";
 
 export const connectionString = `postgres://${config.dbCredentials.user}:${config.dbCredentials.password}@${config.dbCredentials.host}/${config.dbCredentials.database}`;
 
 const connection = postgres(connectionString);
+
+export const db = drizzle(connection, { schema });
 
 export default connection;
--- Server/src/Models/userModel.ts
+++ Server/src/Models/userModel.ts
@@ -1,22 +1,19 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import * as schema from "./Schema/databaseSchema";
-import connection from "../../Database/connection";
+import { db } from "../../Database/connection";
 import { eq, sql } from "drizzle-orm";
-
-const db = drizzle(connection, { schema });
+import { user } from "./Schema/databaseSchema";
 
 export function getAllUsers() {}
 
 export function getUserById(id: number) {}
 
 export function getUserByEmail(email: string) {
-  return db.select().from(schema.user).where(eq(schema.user.email, email));
+  return db.select().from(user).where(eq(user.email, email));
 }
 
 export function updateUser<User>(data: User) {}
 
 export function storeUser(data: UserRegister) {
-  return db.insert(schema.user).values(data);
+  return db.insert(user).values(data);
 }
 
 export function deleteUser<User>(data: User) {}
